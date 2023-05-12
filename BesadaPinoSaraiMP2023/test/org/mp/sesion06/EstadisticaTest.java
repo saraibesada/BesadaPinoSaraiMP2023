b2977bc51169b5945d1ad52ec184d018eb97066c
Sarai Besada <sbp937@inlumine.ual.es>
2023-05-12 10:29:58 +0200

    GTA1S0841 Librería math3 y sesión 06

--- BesadaPinoSaraiMP2023/test/org/mp/sesion06/EstadisticaTest.java
+++ BesadaPinoSaraiMP2023/test/org/mp/sesion06/EstadisticaTest.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 import org.junit.Before;
 import org.junit.Test;
 
-public class EstadisticaTestA {
+public class EstadisticaTest {
 	String archivoTexto;
 	String directorioEntrada;
 	ConjuntoDatos cd; 
